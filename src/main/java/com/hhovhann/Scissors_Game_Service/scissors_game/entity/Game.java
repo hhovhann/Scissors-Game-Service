1c68d04dbf7bfa7bd3b5fb3088dcd1d6681da6ae
hhovhann <haik.hovhanisyan@gmail.com>
2024-09-12 19:39:36 +0400

    Fix compilation errors

--- src/main/java/com/hhovhann/Scissors_Game_Service/scissors_game/entity/Game.java
+++ src/main/java/com/hhovhann/Scissors_Game_Service/scissors_game/entity/Game.java
@@ -6,6 +6,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -30,6 +31,7 @@ public class Game {
     @Column(nullable = false)
     private String result;
     @Column(nullable = false)// WIN, LOSE, DRAW
+    @Builder.Default
     private String status = "ACTIVE";   // defaulting to ACTIVE
     @Column(nullable = false)
     private LocalDateTime timestamp;
